b44f244128484c04b407cf47a2037e410a35250a
kashif siddique <mkashifsiddique98@gmail.com>
2025-09-04 04:57:37 +0100

    latest version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
-
+// add some changes
 interface JWTPayload {
   role?: string;
   pages?: string[];
